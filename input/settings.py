7e0be9504974de73e7e9cc0e09570acdc6d49dde
DAcoder1234 <162916958+DAcoder1234@users.noreply.github.com>
2025-01-21 18:54:24 -0500

    Update settings.py

--- input/settings.py
+++ input/settings.py
@@ -67,10 +67,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 	
 ]
 
 ROOT_URLCONF = 'input.urls'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
